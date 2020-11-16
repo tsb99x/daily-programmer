4a463a513b0b36e68cd46f91d49905823c441733
Anton Muravyov <tsb99x@gmail.com>
2020-11-16 19:35:27 +0300

    Implement 1-easy in Haskell

---
+++ 1-easy-input.hs
@@ -0,0 +1,46 @@
+{-
+
+  [easy] challenge #1
+  create a program that will ask the users name, age, and reddit username.
+  have it tell them the information back, in the format:
+
+    your name is (blank), you are (blank) years old, and your username is (blank)
+
+  for extra credit, have the program log this information in a file to be accessed later.
+
+-}
+
+import System.IO (hFlush, stdout)
+
+main :: IO()
+main =
+    input >>= formMsg >>= output
+
+input :: IO (String, String, String)
+input = do
+    name <- prompt "input your name"
+    age <- prompt "input your age"
+    username <- prompt "input your username"
+    return (name, age, username)
+
+formMsg :: (String, String, String) -> IO String
+formMsg (name, age, username) =
+    return $ concat
+        [ "your name is ", name, ", "
+        , "you are ", age, " years old, "
+        , "and your username is ", username
+        , "\n"
+        ]
+
+output :: String -> IO ()
+output msg = do
+    putStr msg
+    appendFile "tmp/log.txt" msg
+
+-- UTILS
+
+prompt :: String -> IO String
+prompt msg = do
+    putStr $ msg ++ " > "
+    hFlush stdout
+    getLine
