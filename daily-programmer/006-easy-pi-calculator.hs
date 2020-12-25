85ab5daa2e3cf3537cba7d60edbd847a0db034cc
Anton Muravyov <tsb99x@gmail.com>
2020-12-25 17:39:54 +0300

    Small cleanup

--- daily-programmer/006-easy-pi-calculator.hs
+++ daily-programmer/006-easy-pi-calculator.hs
@@ -10,30 +10,29 @@
 
 -}
 
+-- This is an implementation of the Pi Spigot Algorithm
+
 import Data.List (unfoldr)
 
 main :: IO ()
 main = putStrLn $ piDigits 30
 
-{- This is an implementation of the Pi Spigot Algorithm -}
-
 piDigits :: Int -> String
-piDigits n = putComma . convert . feedDigits $ initialize
+piDigits n = putComma . convert . feedDigits $ initialize len
     where len = (10 * n) `div` 3
+          initialize = flip replicate 2
           feedDigits = take n . unfoldr (Just . digit)
-          initialize = replicate len 2
           convert = concatMap show
           putComma = ("3," ++) . tail
 
 digit :: [Int] -> (Int, [Int])
 digit = weave . index . multiply
-    where index = zip [0..]
-          multiply = map (*10)
+    where multiply = map (*10)
+          index = zip [0..]
           weave = foldr calc (0, [])
 
 calc :: (Int, Int) -> (Int, [Int]) -> (Int, [Int])
 calc (i, x) (carry, rems) = (quo, rem:rems)
     where val = x + carry * (i + 1)
           den = if i == 0 then 10 else (2 * i) + 1
-          rem = val `mod` den
-          quo = val `div` den
+          (quo, rem) = val `divMod` den
