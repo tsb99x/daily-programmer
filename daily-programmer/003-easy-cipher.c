70d077197f15af06587940946ba08e0c044b1e02
Anton Muravyov <tsb99x@gmail.com>
2021-05-19 22:07:34 +0300

    Implement 003-easy in C

---
+++ daily-programmer/003-easy-cipher.c
@@ -0,0 +1,44 @@
+#include "utils.h"
+
+#define BUF_SIZE 1024
+
+char rotcb(
+    char c,
+    char base,
+    int shift,
+    int alpha_len
+) {
+    return base + (c - base + shift) % alpha_len;
+}
+
+char rotc(
+    char c,
+    int shift
+) {
+    if (c >= 'a' && c <= 'z')
+        return rotcb(c, 'a', shift, 26);
+    if (c >= 'A' && c <= 'Z')
+        return rotcb(c, 'A', shift, 26);
+    return c;
+}
+
+void rots(
+    char *s,
+    int shift
+) {
+    while (*s)
+        *s++ = rotc(*s, shift);
+}
+
+int main(
+    void
+) {
+    char in[BUF_SIZE];
+
+    input("string to encode > ", in, sizeof(in));
+    rots(in, 13);
+    printf("rot13 is '%s'\n", in);
+    rots(in, 13);
+    printf("rot13 is '%s'\n", in);
+    return 0;
+}
