13c7db638f0da3329f48b9c2b8b32a764614a85f
Anton Muravyov <tsb99x@gmail.com>
2021-05-19 22:08:07 +0300

    Implement 004-easy in C

---
+++ daily-programmer/004-easy-password-generator.c
@@ -0,0 +1,58 @@
+#include "utils.h"
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+void panic(
+    const char *msg
+) {
+    fputs(msg, stderr);
+    fputc('\n', stderr);
+    exit(EXIT_FAILURE);
+}
+
+bool convert_s_to_i(
+    const char *s,
+    int *i
+) {
+    char *p;
+
+    *i = strtol(s, &p, 10);
+    return p == s + strlen(s);
+}
+
+int read_int(
+    const char *prompt
+) {
+    char in[1024];
+    int res;
+
+    input(prompt, in, sizeof(in));
+    if (!convert_s_to_i(in, &res))
+        panic("cannot convert string to integer");
+    return res;
+}
+
+int main(
+    void
+) {
+    int i, j, len, cnt, alpha_len;
+    const char alpha[] =
+        "abcdefghijklmnopqrstuvwxyz"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "1234567890"
+        ".,-=+@$^&:;";
+
+    srand(time(NULL));
+
+    len = read_int("password length > ");
+    cnt = read_int("passwords count > ");
+
+    alpha_len = strlen(alpha);
+    for (i = 0; i < cnt; i++, putchar('\n'))
+        for (j = 0; j < len; j++)
+            putchar(alpha[rand() % alpha_len]);
+
+    return EXIT_SUCCESS;
+}
