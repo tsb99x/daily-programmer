8b204e3c21116947fbe88350c37810b53c911cd9
Anton Muravyov <tsb99x@gmail.com>
2021-05-20 22:58:38 +0300

    Extract 001-easy rules

--- daily-programmer/001-easy-input.c
+++ daily-programmer/001-easy-input.c
@@ -1,18 +1,3 @@
-/*
-
-    https://www.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/
-
-    [easy] challenge #1
-
-    create a program that will ask the users name, age, and reddit username.
-    have it tell them the information back, in the format:
-
-        your name is (blank), you are (blank) years old, and your username is (blank)
-
-    for extra credit, have the program log this information in a file to be accessed later.
-
-*/
-
 #include "utils.h"
 
 #define LENGTH 256
