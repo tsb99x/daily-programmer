17c58ad3919947b459553adfcc3af400807b7156
Anton Muravyov <tsb99x@gmail.com>
2021-05-22 19:11:39 +0300

    Extract 004-easy rules

--- daily-programmer/004-easy-password-generator.hs
+++ daily-programmer/004-easy-password-generator.hs
@@ -1,17 +1,3 @@
-{-
-
-    https://www.reddit.com/r/dailyprogrammer/comments/pm6oj/2122012_challenge_4_easy/
-
-    [2/12/2012] Challenge #4 [easy]
-
-    You're challenge for today is to create a random password generator!
-
-    For extra credit, allow the user to specify the amount of passwords to generate.
-
-    For even more extra credit, allow the user to specify the length of the strings he wants to generate!
-
--}
-
 import System.IO (hFlush, stdout)
 import Text.Read (readMaybe)
 import System.Random (getStdGen, randomRs, StdGen)
