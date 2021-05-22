6bb64a0188d2a6beef97d5fd3aedd9a10d47ab1d
Anton Muravyov <tsb99x@gmail.com>
2021-05-22 16:02:03 +0300

    Extract 003-easy rules

--- daily-programmer/003-easy-cipher.hs
+++ daily-programmer/003-easy-cipher.hs
@@ -1,18 +1,3 @@
-{-
-
-    https://www.reddit.com/r/dailyprogrammer/comments/pkw2m/2112012_challenge_3_easy/
-
-    [2/11/2012] Challenge #3 [easy]
-
-    Welcome to cipher day!
-
-    write a program that can encrypt texts with an alphabetical caesar cipher.
-    This cipher can ignore numbers, symbols, and whitespace.
-
-    for extra credit, add a "decrypt" function to your program!
-
--}
-
 import System.IO (hFlush, stdout)
 import Control.Monad ((<$!>))
 import Text.Read (readMaybe)
