3e617193deca9abc9212dcd96468e57f530af75a
Anton Muravyov <tsb99x@gmail.com>
2021-05-23 19:44:09 +0300

    Extract 005-easy rules

--- daily-programmer/005-easy-login.hs
+++ daily-programmer/005-easy-login.hs
@@ -1,17 +1,3 @@
-{-
-
-    https://www.reddit.com/r/dailyprogrammer/comments/pnhyn/2122012_challenge_5_easy/
-
-    [2/12/2012] Challenge #5 [easy]
-
-    Your challenge for today is to create a program which is password protected, and wont open unless the correct user and password is given.
-
-    For extra credit, have the user and password in a seperate .txt file.
-
-    for even more extra credit, break into your own program :)
-
--}
-
 {-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
 
 import Utils (input)
